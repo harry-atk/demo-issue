87941096681bd2c989b577ad2191a7ebd05aeb6a
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2024-01-14 20:35:12 -0500

    description

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,8 +1,10 @@
 import Button from 'ui/button'
+// imports work as expected, but has no add import quick fix 
 
 export default function Home() {
   return (
     <main>
+      {/* typing "<Frame" shows no import intellisense */}
       <Button></Button>
     </main>
   )
